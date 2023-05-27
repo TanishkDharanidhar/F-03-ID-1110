d4e73ed30c2c7170f2169d67db147446ef9b8319
TanishkDharanidhar <114863262+TanishkDharanidhar@users.noreply.github.com>
2023-05-27 19:21:18 +0530

    Update main_code_logic.py

--- main_code_logic.py
+++ main_code_logic.py
@@ -51,30 +51,6 @@ def alphachecker(user_word):
    else:
        return False
 
-keyb_pre = {}
-for i in range(1, 27):  #ASCII 65-90 upper case
-   key = i
-   value = chr(i + 64)  # Convert the number to its corresponding uppercase alphabet ASCII value
-   keyb_pre[key] = value
-keyb = keyb_pre
-
-def keyboard(user_word,correct_word):
-   global keyb
-   user_word = user_word.lower()
-   for i,c in enumerate(user_word):
-       if c in correct_word:
-           if user_word[i] == correct_word[i]:
-               temp = Fore.WHITE + Back.GREEN + user_word[i].upper() + Style.RESET_ALL
-               keyb[(ord(user_word[i]) - 96)] = temp
-           else:
-               temp = Fore.BLUE + Back.YELLOW + user_word[i].upper() + Style.RESET_ALL
-               keyb[(ord(user_word[i]) - 96)] = temp
-       else:
-           temp = Fore.WHITE + Back.CYAN + user_word[i].upper() + Style.RESET_ALL
-           keyb[(ord(user_word[i]) - 96)] = temp
-   return f"{keyb[17]}  {keyb[23]}  {keyb[5]}  {keyb[18]}  {keyb[20]}  {keyb[25]}  {keyb[21]}  {keyb[9]}  {keyb[15]}  {keyb[16]}\n {keyb[1]}  {keyb[19]}  {keyb[4]}  {keyb[6]}  {keyb[7]}  {keyb[8]}  {keyb[10]}  {keyb[11]}  {keyb[12]}\n  {keyb[26]}  {keyb[24]}  {keyb[3]}  {keyb[22]}  {keyb[2]}  {keyb[14]}  {keyb[13]}"
-#user_word = 'Hbppa'
-correct_word = 'happy'
 #print(word_check(user_word,correct_word))
 #print('abc')
 #a= Fore.WHITE + Back.GREEN + 'A'
@@ -89,30 +65,97 @@ def random_word():
         words = f.read().splitlines()
         return random.choice(words)
 
-guess_number = 0
-#correct_word = random_word()
-print('Enter 5-letter word.')
+def hint_random_word():
+    with open("random_word_hint.txt") as f:
+        words = f.read().splitlines()
+        return random.choice(words)
 
-while guess_number < 6:
-   user_word = input()[:5] # TODO: error handling
-   if alphachecker(user_word) == False:
-       print(f'{user_word} Word should contain only alphabets. Try again.')
-       continue
-   if len(user_word) < 5:
-       print(f'{user_word} Please enter 5-letter word. Try again.')
-       continue
-   check = word_check(user_word, correct_word)
-   print(check)
-   print(keyboard(user_word,correct_word))
-   correct_answer_check = color_count(user_word, correct_word)
-   if correct_answer_check[0] == 5:
-       print('Correct answer!')
-       break
-   else:
-       guess_number += 1
+play = True
 
-if guess_number == 6:
-   print(f"Your 6 guesses are over. It was '{correct_word}'.")
+while play:
+    keyb_pre = {}
+    for i in range(1, 27):  #ASCII 65-90 upper case
+        key = i
+        value = chr(i + 64)  # Convert the number to its corresponding uppercase alphabet ASCII value
+        keyb_pre[key] = value
+    keyb = keyb_pre
+
+    def keyboard(user_word,correct_word):
+        global keyb
+        user_word = user_word.lower()
+        for i,c in enumerate(user_word):
+            if c in correct_word:
+                if user_word[i] == correct_word[i]:
+                    temp = Fore.WHITE + Back.GREEN + user_word[i].upper() + Style.RESET_ALL
+                    keyb[(ord(user_word[i]) - 96)] = temp
+                else:
+                    temp = Fore.BLUE + Back.YELLOW + user_word[i].upper() + Style.RESET_ALL
+                    keyb[(ord(user_word[i]) - 96)] = temp
+            else:
+                temp = Fore.WHITE + Back.CYAN + user_word[i].upper() + Style.RESET_ALL
+                keyb[(ord(user_word[i]) - 96)] = temp
+        return f"{keyb[17]}  {keyb[23]}  {keyb[5]}  {keyb[18]}  {keyb[20]}  {keyb[25]}  {keyb[21]}  {keyb[9]}  {keyb[15]}  {keyb[16]}\n {keyb[1]}  {keyb[19]}  {keyb[4]}  {keyb[6]}  {keyb[7]}  {keyb[8]}  {keyb[10]}  {keyb[11]}  {keyb[12]}\n  {keyb[26]}  {keyb[24]}  {keyb[3]}  {keyb[22]}  {keyb[2]}  {keyb[14]}  {keyb[13]}"
+
+    guess_number = 0
+    correct_word = 'happy'
+    #correct_word = random_word()
+    print('Enter 5-letter word.')
+    print("If you need help during an attempt, type 'help'.")
+    while guess_number < 6:
+        user_word = input()[:5] # TODO: error handling
+
+        if user_word == 'help':
+            #help menu: random word, vowels present, starting letter ending letter
+            print('1 Random Word: Get a random word from a list of words.\n2 Vowels Present: Get the vowels present in the answer.\n3 Starting Letter: Get the starting letter of the answer.\n4 Ending Letter: Get the ending letter of the answer.')
+            hint_response = input('Type number: ')
+            if hint_response == '1':
+                #random word
+                print("Random word = ",hint_random_word().lower())
+                continue
+            elif hint_response == '2':
+                #vowels present
+                vowels = 'aeiou'
+                correct_word_vowels = ''
+                for i in correct_word:
+                    if i in vowels:
+                        temp = i.upper() + ' '
+                        correct_word_vowels += temp
+                print('Vowels present: ', correct_word_vowels)
+                continue
+            elif hint_response == '3':
+                #starting letter
+                print('Starting letter: ', correct_word[0])
+                continue
+            elif hint_response == '4':
+                #ending letter
+                print('Ending letter: ', correct_word[-1])
+                continue
+        else:
+            if alphachecker(user_word) == False:
+                print(f'{user_word} Word should contain only alphabets. Try again.')
+                continue
+
+            if len(user_word) < 5:
+                print(f'{user_word} Please enter 5-letter word. Try again.')
+                continue
+
+            check = word_check(user_word, correct_word)
+            print(check)
+            print(keyboard(user_word,correct_word))
+
+            correct_answer_check = color_count(user_word, correct_word)
+            if correct_answer_check[0] == 5:
+                print('Correct answer!')
+                break
+            else:
+                #hint
+                guess_number += 1
+
+    if guess_number == 6:
+        print(f"Your 6 guesses are over. It was '{correct_word}'.")
+
+    response = input("Do you want to play again? Type 'yes' or 'no'.\n")
+    if response == 'no':
+        play = False
 #clean code
 #add comments
-
